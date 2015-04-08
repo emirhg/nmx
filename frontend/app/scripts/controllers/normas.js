3766229a98c319195853d76a0584a7b3c06a7107
Hugo Mosh <hugomosh@gmail.com>
2015-04-08 18:37:09 -0500

    Fix en nombres

--- frontend/app/scripts/controllers/normas.js
+++ frontend/app/scripts/controllers/normas.js
@@ -8,87 +8,87 @@
  * Controller of the frontendApp
  */
 angular.module('frontendApp')
-	.controller('NormasCtrl', function($scope, $location, datos, $routeParams) {
-		$scope.listaTabs = [{
-			titulo: 'NOMs Vigentes',
-			clave: 'vigente'
-		}, {
-			titulo: 'Proyectos de NOM',
-			clave: 'proyecto'
-		}, {
-			titulo: 'NOMs Canceladas',
-			clave: 'cancelada'
-		}];
-		$scope.accederNorma = function accederNorma(claveNOM) {
-			console.log('accederNorma' + claveNOM);
-			$location.path('/nom/' + claveNOM);
-		};
-		$scope.listadoNOMsActual = [];
-		datos.getListadoNOMS().then(function(datos) {
-			$scope.listadoNOMsActual = datos;
-		});
-		$scope.normaActual = {};
+    .controller('NormasCtrl', function($scope, $location, datos, $routeParams) {
+        $scope.listaTabs = [{
+            titulo: 'NOMs Vigentes',
+            clave: 'vigente'
+        }, {
+            titulo: 'Proyectos de NOM',
+            clave: 'proyecto'
+        }, {
+            titulo: 'NOMs Canceladas',
+            clave: 'cancelada'
+        }];
+        $scope.accederNorma = function accederNorma(claveNOM) {
+            console.log('accederNorma' + claveNOM);
+            $location.path('/nom/' + encodeURIComponent(claveNOM));
+        };
+        $scope.listadoNOMsActual = [];
+        datos.getListadoNOMS().then(function(datos) {
+            $scope.listadoNOMsActual = datos;
+        });
+        $scope.normaActual = {};
 
-		$scope.equivalencias = {
-			'NULL': {
-				icono: 'cog',
-				nombre: 'Sin categorizar',
-				categoria: 'nulo'
-			},
-			'PROY Modificación': {
-				icono: 'lab',
-				nombre: 'Proyecto de modificación',
-				categoria: 'proymod'
-			},
-			'PROY NOM': {
-				icono: 'bulb',
-				nombre: 'Proyecto de norma',
-				categoria: 'proymnom'
-			},
-			'Otros Documentos': {
-				icono: 'attachment',
-				nombre: 'Otros',
-				categoria: 'otros'
-			},
-			'NOM': {
-				icono: 'note',
-				nombre: 'Publicación de Norma',
-				categoria: 'nom'
-			},
-			'Respuestas': {
-				icono: 'bullhorn',
-				nombre: 'Respuestas a comentarios',
-				categoria: 'resp'
-			},
-			'Modificación': {
-				icono: 'quill',
-				nombre: 'Modicación',
-				categoria: 'mod'
-			}
-		};
-		$scope.seleccionaIcono = function selec(tipo) {
-			return $scope.equivalencias[tipo].icono;
+        $scope.equivalencias = {
+            'NULL': {
+                icono: 'cog',
+                nombre: 'Sin categorizar',
+                categoria: 'nulo'
+            },
+            'Proyecto Modificación': {
+                icono: 'lab',
+                nombre: 'Proyecto de modificación',
+                categoria: 'proymod'
+            },
+            'Proyecto NOM': {
+                icono: 'bulb',
+                nombre: 'Proyecto de norma',
+                categoria: 'proymnom'
+            },
+            'Otros Documentos': {
+                icono: 'attachment',
+                nombre: 'Otros',
+                categoria: 'otros'
+            },
+            'NOM': {
+                icono: 'note',
+                nombre: 'Publicación de Norma',
+                categoria: 'nom'
+            },
+            'Respuestas a Comentarios': {
+                icono: 'bullhorn',
+                nombre: 'Respuestas a comentarios',
+                categoria: 'resp'
+            },
+            'Modificación': {
+                icono: 'quill',
+                nombre: 'Modicación',
+                categoria: 'mod'
+            }
+        };
+        $scope.seleccionaIcono = function selec(tipo) {
+            return $scope.equivalencias[tipo].icono;
 
 
-		};
-		$scope.seleccionaCategoriaNOM = function selec(tipo) {
-			return $scope.equivalencias[tipo].categoria;
+        };
+        $scope.seleccionaCategoriaNOM = function selec(tipo) {
+            return $scope.equivalencias[tipo].categoria;
 
-		};	
-		$scope.seleccionaNombreNOM = function selec(tipo) {
-			return $scope.equivalencias[tipo].nombre;
+        };
+        $scope.seleccionaNombreNOM = function selec(tipo) {
+            return $scope.equivalencias[tipo].nombre;
 
-		};
+        };
 
-		$scope.guionRex = /-/g;
+        $scope.guionRex = /-/g;
 
-		if ($routeParams.clave) {
-			$scope.claveActual = $routeParams.clave;
-			datos.getNOM($routeParams.clave).then(function(datos1) {
-				$scope.normaActual = datos1;
-				datos.getNOMgeneral($routeParams.clave).then(function(datos2) {
-					$scope.normaActualDetalle = datos2[0];
-				});
-			});
-		}
-	});
+        if ($routeParams.clave) {
+            $scope.claveActual = decodeURIComponent($routeParams.clave);
+            datos.getNOM($routeParams.clave).then(function(datos1) {
+                $scope.normaActual = datos1;
+                datos.getNOMgeneral($routeParams.clave).then(function(datos2) {
+                    $scope.normaActualDetalle = datos2[0];
+                });
+            });
+        }
+    });
