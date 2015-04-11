5fbb533e86b2369033c029bc38132b26ba4fe9e0
Emir Herrera <emir.herrera@imco.org.mx>
2015-04-10 22:48:08 -0500

    Refs #39 @3h Homologación de la nomenclatura en claves de NOMs

---
+++ database/004-notasnomPatch.sql
@@ -0,0 +1,28 @@
+------------ Correcciones en claves nom (Intervención humana) -----
+update notasnom set clavenomnorm = replace(clavenomnorm,'NOMEM', 'NOM-EM') where clavenomnorm like 'NOMEM%';
+
+update notasnom set clavenom = 'NOM-023-   PESC-1996', clavenomnorm='NOM-023-PESC-1996' where clavenomnorm = 'NOM-023-';
+update notasnom set clavenom = 'NOM-016-   PESC-1994', clavenomnorm='NOM-016-PESC-1994' where clavenomnorm = 'NOM-016-';
+update notasnom set clavenom = 'NOM-055-1979', clavenomnorm='NOM-055-1979' where clavenomnorm = 'NO.NOM-055-1979';
+update notasnom set clavenom = 'NOM-021-SSA2 - 1994', clavenomnorm = 'NOM-021-SSA2-1994' WHERE clavenomnorm='NOM-021-SSA2';
+update notasnom set clavenom = 'NOM-038-SSA I-1993', clavenomnorm = 'NOM-038-SSA1-1993' WHERE clavenomnorm='NOM-038-SS';
+update notasnom set clavenom = 'NOM - 115 - STPS - 1994', clavenomnorm = 'NOM-115-STPS-1994' WHERE clavenomnorm='NOM-115';
+update notasnom set clavenom = 'NOM-243-   SSA1-2010', clavenomnorm = 'NOM-243-SSA1-2010' WHERE clavenomnorm='NOM-243-';
+update notasnom set clavenom = 'NOM-039-SSA I-1993', clavenomnorm = 'NOM-039-SSA|-1993' WHERE clavenomnorm='NOM-039-SS';
+update notasnom set clavenomnorm = 'NOM-AA-005-1980' WHERE clavenomnorm='NOM-AA-005-198O';
+
+update notasnom set clavenomnorm = regexp_replace(clavenomnorm, '[LI](\d{3})','1\1') WHERE EXISTS (select regexp_matches(clavenomnorm,'[LI]\d{3}$'));
+
+
+
+
+update notasnom set clavenom = 'NOM-024-SCFI-  \f1998', clavenomnorm = 'NOM-024-SCFI-1998' WHERE clavenomnorm='NOM-024-SCFI-';
+update notasnom set clavenom = 'NOM-033-SCFI', clavenomnorm = 'NOM-033-SCFI-1994', etiqueta= 'Cancelación', revisionHumana=true WHERE clavenomnorm='NOM-033-SCF';
+
+
+
+update notasnom set clavenom = 'NOM-243-   SSA1-2010', clavenomnorm = 'NOM-243-SSA1-2010' WHERE clavenomnorm='NOM-243-';
+update notasnom set clavenom = 'NOM-243-   SSA1-2010', clavenomnorm = 'NOM-243-SSA1-2010' WHERE clavenomnorm='NOM-243-';
+update notasnom set clavenom = 'NOM-243-   SSA1-2010', clavenomnorm = 'NOM-243-SSA1-2010' WHERE clavenomnorm='NOM-243-';
+update notasnom set clavenom = 'NOM-243-   SSA1-2010', clavenomnorm = 'NOM-243-SSA1-2010' WHERE clavenomnorm='NOM-243-';
+
