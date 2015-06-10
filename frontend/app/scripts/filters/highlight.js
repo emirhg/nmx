2540d745affe5a22a07443743d9b741fcf93b94c
hugomosh <hugomosh@gmail.com>
2015-06-10 13:06:33 -0500

    closes #252 refs #252 @1h filros y busqueda con highlight pra resultados

---
+++ frontend/app/scripts/filters/highlight.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name frontendApp.filter:highlight
+ * @function
+ * @description
+ * # highlight
+ * Filter in the frontendApp.
+ */
+angular.module('frontendApp')
+    .filter('highlight', function($sce) {
+        console.log('highlight');
+        return function(text, phrase) {
+            if (phrase) text = text.replace(new RegExp('(' + phrase + ')', 'gi'),
+                '<span class="highlighted">$1</span>')
+            return $sce.trustAsHtml(text)
+        }
+    });
